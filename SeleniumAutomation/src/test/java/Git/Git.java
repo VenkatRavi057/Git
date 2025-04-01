be55035201d05a0525aa56f03e2f3331cf7728b3
venkat <venkatravi5332@gmail.com>
2025-04-01 11:19:21 +0100

    pushing after the update from master

--- SeleniumAutomation/src/test/java/Git/Git.java
+++ SeleniumAutomation/src/test/java/Git/Git.java
@@ -11,6 +11,8 @@ public class Git {
 		System.out.println("Message from master at 11.00 AM of GMT");
 		
 		System.out.println("Message from develop after the pull from master!!");
+		
+		System.out.println("Message from master after the pull from develop and pushing at 11.18 AM at GMT ");
 
 	}
 }
